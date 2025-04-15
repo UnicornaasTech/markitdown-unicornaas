0466468190cd51539553b6913f4c7c64f32a4a4b
Panu Hällfors <panu.hallfors@unicornaas.com>
2025-04-15 13:56:01 +0200

    Created a separate "unicornaas" setup for dependencies. Commented out libraries not needed in unicornaas setup.

--- packages/markitdown/src/markitdown/converters/__init__.py
+++ packages/markitdown/src/markitdown/converters/__init__.py
@@ -2,26 +2,27 @@
 #
 # SPDX-License-Identifier: MIT
 
+# Changed for unicornaas: only importing converters that are needed for the current environment
 from ._plain_text_converter import PlainTextConverter
 from ._html_converter import HtmlConverter
 from ._rss_converter import RssConverter
 from ._wikipedia_converter import WikipediaConverter
-from ._youtube_converter import YouTubeConverter
-from ._ipynb_converter import IpynbConverter
+#from ._youtube_converter import YouTubeConverter
+#from ._ipynb_converter import IpynbConverter
 from ._bing_serp_converter import BingSerpConverter
 from ._pdf_converter import PdfConverter
 from ._docx_converter import DocxConverter
 from ._xlsx_converter import XlsxConverter, XlsConverter
 from ._pptx_converter import PptxConverter
-from ._image_converter import ImageConverter
-from ._audio_converter import AudioConverter
+#from ._image_converter import ImageConverter
+#from ._audio_converter import AudioConverter
 from ._outlook_msg_converter import OutlookMsgConverter
-from ._zip_converter import ZipConverter
-from ._doc_intel_converter import (
-    DocumentIntelligenceConverter,
-    DocumentIntelligenceFileType,
-)
-from ._epub_converter import EpubConverter
+#from ._zip_converter import ZipConverter
+#from ._doc_intel_converter import (
+#    DocumentIntelligenceConverter,
+#    DocumentIntelligenceFileType,
+#)
+#from ._epub_converter import EpubConverter
 from ._csv_converter import CsvConverter
 
 __all__ = [
@@ -29,20 +30,20 @@ __all__ = [
     "HtmlConverter",
     "RssConverter",
     "WikipediaConverter",
-    "YouTubeConverter",
-    "IpynbConverter",
+    #"YouTubeConverter",
+    #"IpynbConverter",
     "BingSerpConverter",
     "PdfConverter",
     "DocxConverter",
     "XlsxConverter",
     "XlsConverter",
     "PptxConverter",
-    "ImageConverter",
-    "AudioConverter",
+    #"ImageConverter",
+    #"AudioConverter",
     "OutlookMsgConverter",
-    "ZipConverter",
-    "DocumentIntelligenceConverter",
-    "DocumentIntelligenceFileType",
-    "EpubConverter",
+    #"ZipConverter",
+    #"DocumentIntelligenceConverter",
+    #"DocumentIntelligenceFileType",
+    #"EpubConverter",
     "CsvConverter",
 ]
